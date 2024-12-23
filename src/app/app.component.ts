80a801d73ca6c59294f5bfcfdab6f636680ef5e1
gadamaga <gadamagamyradov08@gmail.com>
2024-12-23 14:06:53 +0500

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent,],
+  imports: [HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'transfer_angular';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-
+import { ContactusComponent } from './contactus/contactus.component';
 export const routes: Routes = [
-  
+  { path: 'contactus', component: ContactusComponent },
 ];
