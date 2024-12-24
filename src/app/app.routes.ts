d2ac448193d6b00f99decd584da6d8201dc733a7
gadamaga <gadamagamyradov08@gmail.com>
2024-12-25 00:10:14 +0500

    3'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
+// filepath: /C:/Users/G.AA/Desktop/transfer_angular/src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { ContactusComponent } from './contactus/contactus.component';
 
 export const routes: Routes = [
-  
+  { path: 'contactus', component: ContactusComponent },
 ];
