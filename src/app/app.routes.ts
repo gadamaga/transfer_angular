1d4492d73dfc2eccb971a6d9a89fb02e45a91ddd
Amanmyrat <amansarahs@gmail.com>
2024-12-24 20:10:34 +0500

    app.routes.ts was modified'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,17 @@
 import { Routes } from '@angular/router';
 import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+import { PriceListComponent } from './price-list/price-list.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { GalleryComponent } from './gallery/gallery.component';
 export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'pricelist', component: PriceListComponent},
+  {path: 'aboutus', component: AboutusComponent},
   { path: 'contactus', component: ContactusComponent },
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'resevation', component: ReservationComponent}
 ];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,7 +4,9 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink, 
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
