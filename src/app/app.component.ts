fe93cb2bc9074fc8012d875bb4b9b1f627569e90
gadamaga <gadamagamyradov08@gmail.com>
2024-12-30 00:48:36 +0500

    cotnact_info gosdum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent,HomeComponent, RouterModule,],
+  imports: [HeaderComponent, FooterComponent,HomeComponent, RouterModule,ContactInfoComponent,],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
